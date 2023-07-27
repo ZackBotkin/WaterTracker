266cce5188e54115a0a13dfd00fb365dee7d1d04
Zack Botkin <zackbotkin@gmail.com>
2023-07-26 23:37:28 -0500

    paths --> path

--- tmp/src/interactive/main_menu.py
+++ tmp/src/interactive/main_menu.py
@@ -4,8 +4,8 @@ from interactive_menu.src.interactive_menu import InteractiveMenu
 
 class MainMenu(InteractiveMenu):
 
-    def __init__(self, manager, paths=[]):
-        super().__init__(manager, paths)
+    def __init__(self, manager, path=[]):
+        super().__init__(manager, path)
         self.sub_menu_modules = []
 
     def title(self):
