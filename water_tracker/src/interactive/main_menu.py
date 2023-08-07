b08e28be4aa79060699f67c3812afab500799533
Zack Botkin <zackbotkin@gmail.com>
2023-08-07 15:13:36 -0500

    allow to work with continued commands input (ie: "record 1" is a thing now)

--- water_tracker/src/interactive/main_menu.py
+++ water_tracker/src/interactive/main_menu.py
@@ -19,37 +19,52 @@ class RecordWaterMenu(InteractiveMenu):
     def title(self):
         return "Record"
 
-    def main_loop(self):
-        form_results = self.interactive_form(
-            [
-                {
-                    "question": "How many glasses of water?",
-                    "expected_response_type": "INT",
-                    "return_as": "count",
-                    "default": "",
-                    "allow_empty": False
-                },
-                {
-                    "question": "What date? (YYYY-MM-DD) Hit enter for today",
-                    "expected_response_type": "YYYYMMDD_Date",
-                    "return_as": "date",
-                    "default": datetime.now().strftime("%Y-%m-%d"),
-                    "allow_empty": False
-                }
-            ]
-        )
-        if form_results["user_accept"] != True:
-            print("Aborting!")
-            return
-        form_results.pop("user_accept")
-        for answer_key in form_results.keys():
-            if not form_results[answer_key]["valid"]:
-                print("%s is not a valid value! Aborting" % answer_key)
-                return
+    def main_loop(self, **kwargs):
 
-        count = form_results["count"]["value"]
-        date = form_results["date"]["value"]
-        self.manager.record_water(count, date)
+        if "continued_commands" in kwargs:
+            continued_commands = kwargs["continued_commands"]
+            if len(continued_commands) == 1:
+                count = continued_commands.pop(0)
+                date = datetime.now().strftime("%Y-%m-%d")
+                self.manager.record_water(count, date)
+            elif len(continued_commands) == 2:
+                count = continued_commands.pop(0)
+                date = continued_commands.pop(1)
+                self.manager.record_water(count, date)
+            else:
+                print("TOO many arguments!!!")
+                print("Aborting")
+        else:
+            form_results = self.interactive_form(
+                [
+                    {
+                        "question": "How many glasses of water?",
+                        "expected_response_type": "INT",
+                        "return_as": "count",
+                        "default": "",
+                        "allow_empty": False
+                    },
+                    {
+                        "question": "What date? (YYYY-MM-DD) Hit enter for today",
+                        "expected_response_type": "YYYYMMDD_Date",
+                        "return_as": "date",
+                        "default": datetime.now().strftime("%Y-%m-%d"),
+                        "allow_empty": False
+                    }
+                ]
+            )
+            if form_results["user_accept"] != True:
+                print("Aborting!")
+                return
+            form_results.pop("user_accept")
+            for answer_key in form_results.keys():
+                if not form_results[answer_key]["valid"]:
+                    print("%s is not a valid value! Aborting" % answer_key)
+                    return
+
+            count = form_results["count"]["value"]
+            date = form_results["date"]["value"]
+            self.manager.record_water(count, date)
 
 class ReadWaterMenu(InteractiveMenu):
 
